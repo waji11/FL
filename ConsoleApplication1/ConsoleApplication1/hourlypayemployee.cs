a831d7f1a82eea3815e0224b699c911e29ecb88e
Wajiha Siddiqui <93635056+waji11@users.noreply.github.com>
2021-11-30 13:59:14 +0500

    Final Lab files

---
+++ ConsoleApplication1/ConsoleApplication1/hourlypayemployee.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public class hourlypayemployee : employee
+    {
+        public int hoursworked
+        {
+            get
+            {
+                hoursworked = 4;
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int basepay
+        {
+            get
+            {
+                basepay = 400;
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int unitpay
+        {
+            get
+            {
+                unitpay = 30;
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int overtimepay
+        {
+            get
+            {
+                overtimepay = 250;
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int hourspay
+        {
+            get
+            {
+                hourspay = 20;
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Property
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int grosspay
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void calculate_salary()
+        {
+            Console.WriteLine(grosspay);
+
+                
+
+               
+                
+            throw new System.NotImplementedException();
+        }
+    }
+}
